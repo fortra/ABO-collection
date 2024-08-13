05dd78c90a14fe42248e7435365deaba782679a0
Solid <65661347+solidcls@users.noreply.github.com>
2024-08-13 16:22:13 -0300

    Stack2

---
+++ 01_Stacks/exercises/stack2/STACK2_VS_2017.cpp
@@ -0,0 +1,29 @@
+/* abo4-stdin.c                                                    *
+* specially crafted to feed your brain by gera@core-sdi.com */
+
+/* After this one, the next is just an Eureka! away          */
+
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <stdlib.h>
+#include  <stdio.h> 
+#include "Windows.h"
+
+
+int main(int argc, char **argv) {
+
+
+	MessageBoxA((HWND)-0, (LPCSTR) "Imprimir You win..\n", (LPCSTR)"Vamosss", (UINT)0);
+
+	int cookie;
+	char buf[80];
+
+	printf("buf: %08x cookie: %08x\n", &buf, &cookie);
+	gets(buf);
+
+	if (cookie == 0x01020305)
+		printf("you win!\n");
+
+}
+
